de87853e6b8654e2b641ca7493f51d7102d8d315
Chittakorn18 <153198154+Chittakorn18@users.noreply.github.com>
2024-12-18 15:52:33 +0700

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,28 @@
 
+import java.time.LocalDate;
+
+public class User {
+    private String name;
+    private LocalDate dob;
+    public User(){
+        name = null;
+        dob = LocalDate.now();
+    }
+    public User(String name , int year ,int month , int day){
+        this.name = name;
+        this.dob = LocalDate.of(year, month, month);
+    }
+
+    public String getName(){
+        return name;
+    }
+    public LocalDate getDob(){
+        return dob;
+    }
+
+    public void displayInfo(){
+        System.out.println("Name: "+ name);
+        System.out.println("DOB(y-m-d): "+ dob);
+    }
+
+}
